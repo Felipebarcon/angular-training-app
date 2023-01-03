2dd671cb0cf27b2afd2ea4b5f5cb637664479855
Felipe Barcon <felipe.barcon@gmail.com>
2023-01-03 21:03:43 +0100

    creating home page and button style

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { HomepageComponent } from './homepage/homepage.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent],
+  declarations: [AppComponent, HeaderComponent, HomepageComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
